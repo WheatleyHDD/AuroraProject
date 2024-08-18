27c8d562c380241faac3211254ea4d4e292b3edc
WheatleyHDD <wheatleyhdd@gmail.com>
2024-08-18 23:55:40 +0500

    Level 2

--- Scripts/elevator_room_start.gd
+++ Scripts/elevator_room_start.gd
@@ -1,5 +1,9 @@
 extends CSGBox3D
 
 
+func _ready() -> void:
+	$CanvasLayer/fade/AnimationPlayer.play("fading")
+
+
 func _on_timer_timeout() -> void:
 	$Elevator/AnimationPlayer.play("DoorOpen")
---
+++ Scripts/target.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+@export var door: Node3D
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body is RigidBody3D:
+		var rb: RigidBody3D = body
+		door.send_open_state()
