96ebce7ce46b2e18231cc1cd3130e214332622f4
WheatleyHDD <wheatleyhdd@gmail.com>
2024-08-19 01:41:25 +0500

    Button Extend

--- Scripts/button.gd
+++ Scripts/button.gd
@@ -1,7 +1,10 @@
 extends Node3D
 
 @export var door: Node3D
-var precision = 0.1
+var precision = 0.15
+
+func _ready() -> void:
+	$Top.set_surface_override_material(0, $Top.get_surface_override_material(0).duplicate())
 
 func _physics_process(delta: float) -> void:
 	var has_cube = false
@@ -11,5 +14,19 @@ func _physics_process(delta: float) -> void:
 			has_cube = true
 			break
 	
-	if has_cube: door.send_open_state()
-	else: door.send_close_state()
+	var mat = $Top.get_surface_override_material(0)
+	
+	if has_cube:
+		$Top.position.y = lerp($Top.position.y, 0.03, delta*60*0.04)
+		mat.set(
+			"albedo_color",
+			lerp(mat.get("albedo_color"), Color.CHARTREUSE, delta*60*0.04)
+		)
+		door.send_open_state()
+	else:
+		$Top.position.y = lerp($Top.position.y, 0.096, delta*60*0.04)
+		mat.set(
+			"albedo_color",
+			lerp(mat.get("albedo_color"), Color.CRIMSON, delta*60*0.04)
+		)
+		door.send_close_state()
