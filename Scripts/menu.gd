8145354146b2a8b8338ad78a900515206cf0e82f
WheatleyHDD <wheatleyhdd@gmail.com>
2024-08-20 20:41:09 +0500

    Prefinal changes

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,6 +1,10 @@
 extends Control
 
 
+func _ready() -> void:
+	var mp: AudioStreamPlayer = $"/root/MusicPlayer"
+	mp.get_stream_playback().switch_to_clip_by_name("menu")
+
 
 func _on_new_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
@@ -8,3 +12,11 @@ func _on_new_game_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_settings_pressed() -> void:
+	$"/root/Overlays/Settings".show()
+
+
+func _on_about_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/readme.tscn")
